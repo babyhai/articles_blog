e74f105253621910a15a97fd886c69fc2662f645
babyhai <hllenoby@gmail.com>
2017-03-14 16:02:48 +0800

    article/show

---
+++ db/migrate/20170313074328_create_articles.rb
@@ -0,0 +1,15 @@
+class CreateArticles < ActiveRecord::Migration[5.0]
+  def up
+    create_table :articles do |t|
+      t.string :title
+      t.text :text
+
+      t.timestamps
+    end
+  end
+
+
+  def down
+    drop_table :articles
+  end
+end
